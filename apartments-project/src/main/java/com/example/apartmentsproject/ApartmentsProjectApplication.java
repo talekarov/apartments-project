4e497a360231969f29c88708d9b7b19d08dbccfe
talekarov <38054022+talekarov@users.noreply.github.com>
2021-02-20 16:03:04 +0100

    Add files via upload

---
+++ apartments-project/src/main/java/com/example/apartmentsproject/ApartmentsProjectApplication.java
@@ -0,0 +1,14 @@
+package com.example.apartmentsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApartmentsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApartmentsProjectApplication.class, args);
+	}
+
+}
