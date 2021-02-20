3a910ac2c39a979e85e041efe06da35dbfb47f43
talekarov <38054022+talekarov@users.noreply.github.com>
2021-02-20 16:13:34 +0100

    Add files via upload

---
+++ src/test/java/com/example/apartmentsproject/ApartmentsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apartmentsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApartmentsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
